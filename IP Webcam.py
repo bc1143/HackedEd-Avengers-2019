6de0c8406344353c23209f1b0af35ee2deb3cc20
brendancoutts <bcoutts@ualberta.ca>
2019-01-19 14:28:05 -0700

    hello

--- IP Webcam.py
+++ IP Webcam.py
@@ -1,13 +1,12 @@
-import urllib
+import urllib.request
 import cv2
 import numpy as np 
-url=#url.address
+url = 'http://172.31.21.206:8080/shot.jpg'
 
 
-while True:
-    imgResp=urllib.urlopen(url)
-    imgNp=np.array(bytearray(imgResp.read()).dtype=np.uint8)
-    img=cv2.imdecode(imgNp,-1)
-    cv2.imshow('test',img)
-    if ord('q')==cv2.waitKey(10):
-        exit(0)
+imgResp = urllib.request.urlopen(url)
+imgNp = np.array(bytearray(imgResp.read()), dtype = np.uint8)
+img = cv2.imdecode(imgNp,-1)
+cv2.imshow('test',img)
+cv2.waitKey(10000)
+
