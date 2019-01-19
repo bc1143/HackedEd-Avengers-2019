717f4a25130cf9cfe57c8b3fa2a4d3f5a002528c
dwu9 <dwu@ualberta.ca>
2019-01-19 15:56:43 -0700

    finding the total, finding the love?

--- findtotal.py
+++ findtotal.py
@@ -1,10 +1,28 @@
-def findtotal(filename):
+import string
+
+
+def find_total_line(filename):
+    suggestive_words = ["Total", "Paid"]
     file = open(filename, 'r')
     lines = file.readlines()
-    for line in lines:
-        if "TOTAL" or "Total" in line:
-            print(line)
-        else:
-            print('No total found')
+    for i in range(0, len(lines)):
+        for word in suggestive_words:
+            if word in lines[i]:
+                total_line = lines[i]
+                return total_line
+            else:
+                pass
 
-findtotal('receiptText_test.txt')
+
+def find_total_number(total_line):
+    total_list = []
+    for characters in total_line:
+        if characters in string.digits or characters == ".":
+            total_list.append(characters)
+    total_amount = ''.join(total_list)
+    print(total_amount)
+
+
+interesting_line = find_total_line('receiptText_test.txt')
+
+find_total_number(interesting_line)
