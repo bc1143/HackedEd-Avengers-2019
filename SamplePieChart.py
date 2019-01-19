a5ed9f17e6109a0c3169713e4106ee862f1ef1b9
Damagon <ayaan1@ualberta.ca>
2019-01-19 15:40:17 -0700

    Pie Chart

---
+++ SamplePieChart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+sizes=[100,1,90]
+transaction=['Cash','Credit','Debit']
+cols=['c','m','r','b']
+plt.pie(sizes,labels=transaction, colors=cols,autopct='%1.1f%%',explode=(0,0.1,0))
+plt.title(c'My Transactions')
+plt.show()
