c113772c5e18c39bd8eced0ecb22dc836b5b3cb9
IE99 <i.elmallah55@gmail.com>
2019-01-19 22:52:41 -0700

    improved upon yelp

--- yelp_search.py
+++ yelp_search.py
@@ -1,47 +1,13 @@
-'''
-import json
-'''
-'''
-from pyquery import PyQuery as pq
-import requests as rq
-import xml.etree.ElementTree as ET
-
-place ='blaze+pizza'
-location = 'Edmonton'
-province = 'AB'
-url = "http://www.yelp.com/search?find_desc=" + place + "find_loc=" + location + "+" + province + "+Canada"
-
-yelp = rq.get(url)
-doc = pq(yelp.text)
-print(doc)
-
-links = doc('li.moredetails a')
-print (len(links))
-for link in links:
-    print (pq(link).attr('href'))
-
-'''
-'''
-jsonData = json.loads('yelp_dataset.tar')
-'''
-'''
-import responses
-from yelp.client import Client
-
-MY_API_KEY = "BkeVvBepP5xWd8hfOi_Pud4wx3d1NWAx7XV_oopCygqKDNuJyE1MBr5TqGhNlBf1KM-cVcz05YsyTGkAkeVq73yTbwbER51fVxc9Qq4vGBhwtCQkjZvPP9LBkvNDXHYx" 
-client = Client(MY_API_KEY)
-
-
-YELP_SAN_FRANCISCO = responses.Response(
-    method="GET",
-    url="https://api.yelp.com/v3/businesses/yelp-san-francisco",
-    #json=read_json_file("business_lookup_yelp_san_francisco.json"),
-    #status=200,
-)
-
-'''
 from yelpapi import YelpAPI
+
 city = "Edmonton"
 yelp_api = YelpAPI("BkeVvBepP5xWd8hfOi_Pud4wx3d1NWAx7XV_oopCygqKDNuJyE1MBr5TqGhNlBf1KM-cVcz05YsyTGkAkeVq73yTbwbER51fVxc9Qq4vGBhwtCQkjZvPP9LBkvNDXHYx")
-print(yelp_api.search_query(term="McDonalds", location=city, sort_by='rating', limit = 5))
-
+op = yelp_api.search_query(term="The Bay", location=city, sort_by='rating', limit = 5)
+title2=''
+print(op)
+title1=op['businesses'][0]['categories'][0]['title']
+try:
+    title2=op['businesses'][0]['categories'][1]['title']
+except IndexError:
+    pass
+print(title1, title2)
