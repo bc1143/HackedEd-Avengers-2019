017cd54e876d589cd3ab47e40537deaf3f17d76f
dwu9 <dwu@ualberta.ca>
2019-01-19 18:08:56 -0700

    Love found before pizza time

--- ultimate_finder.py
+++ ultimate_finder.py
@@ -2,8 +2,12 @@ import string
 
 
 def universal_finder(filename, find_list):
-    marked_list = []
-    found_words = []
+    # filename is name of receipt txt file from receipt image
+    # find_list is list of target words
+    marked_list = []  # List of lines that contains target words
+    found_words = []  # List of target words that were triggered
+    # Open file, read line by line and look for target words
+    # Add entire line to marked_list if line contains target word
     file = open(filename, 'r')
     lines = file.readlines()
     for i in range(0, len(lines)):
@@ -16,14 +20,18 @@ def universal_finder(filename, find_list):
 
 
 def find_store_name(filename):
+    # Just looks at the first line of the receipt txt
+    # Pray that it's there
     file = open(filename, 'r')
     store_name = file.readline()
     file.close()
     print(store_name.lower())
-    return store_name.lower()
+    return store_name.lower()  # Turns it lowercase for easier processing
 
 
 def find_payment_method(filename):
+    # Usually only one instance of one of the target words
+    # Doesn't handle case if there is more than of these target words in the file
     suggestive_words = ["CASH", "Cash", "CREDIT", "Credit", "DEBIT", "Debit"]
     total_list, payment_method = universal_finder(filename, suggestive_words)
     print(payment_method[0].lower())
@@ -31,9 +39,14 @@ def find_payment_method(filename):
 
 
 def find_total_number(filename):
+    # Finds instances of target word
+    # Total usually shows up multiple times ie subtotal and total
+    # Stores these possible totals and then picks the higher one
+    # Doesn't work for Tesco receipts
     suggestive_words = ["Total", "Paid", "total", "TOTAL"]
     total_list, found_words = universal_finder(filename, suggestive_words)
-    confusion_matrix = []
+    confusion_matrix = []  # Stores the potential totals
+    # Go through the line, only keep the numbers and decimal points
     for i in range(0, len(total_list)):
         construct_total = []
         for characters in total_list[i]:
@@ -41,11 +54,12 @@ def find_total_number(filename):
                 construct_total.append(characters)
         total_amount = ''.join(construct_total)
         confusion_matrix.append(total_amount)
-    real_total_baby = max(confusion_matrix)
+    real_total_baby = max(confusion_matrix)  # Picks highest total as the real total
     print(real_total_baby)
     return real_total_baby
 
 
+# Testing and debugging
 testing_file = "receiptText_test.txt"
 print('Total Paid: ')
 find_total_number(testing_file)
