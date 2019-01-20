1f573a6f663c0e5b86fdf890d5d0002a7bfc4db9
IE99 <i.elmallah55@gmail.com>
2019-01-19 23:37:30 -0700

    new ipwebcam

--- IPWebcam.py
+++ IPWebcam.py
@@ -1,7 +1,7 @@
 import urllib.request
 import cv2
 import numpy as np 
-url='http://172.31.21.206:8080/shot.jpg?rnd=665065'
+url='http://172.31.21.206:8080/shot.jpg'
 
 
 while True:
@@ -9,5 +9,6 @@ while True:
     imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
     img = cv2.imdecode(imgNp,-1)
     cv2.imshow('test',img)
+    cv2.imwrite('receiptImage.png',img)
     if ord('q')==cv2.waitKey(10):
         exit(0)
