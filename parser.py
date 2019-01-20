847869fd8b51ccab7a53b63c9ce598bad25c6c6b
grantofino <46849124+grantofino@users.noreply.github.com>
2019-01-19 18:43:35 -0700

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,14 @@
+'''
+can only detect the first instance of the date in the form mm/dd/yyyy OR dd february yyyy OR dd Feb yyyy
+'''
+
+import re
+
+string = 'I want to apply for leaves from 12/12/2017 to 12/18/2017 I want to apply for leaves from 12 January 2017 to ' \
+       '12/18/2017 I want to apply for leaves from 12/12/2017 to 12 Jan 17 '
+
+matches = re.findall('(\d{2}[\/ ](\d{2}|January|Jan|February|Feb|March|Mar|April|Apr|May|May|June|Jun|July|Jul|August|Aug|September|Sep|October|Oct|November|Nov|December|Dec)[\/ ]\d{2,4})', string)
+
+for match in matches:
+    print(match[0])
+    break
